cda59d00cbb5c1af69ddd3a42bbbbf0c1e94c4a9
Erik Aouizerate <erik.aouizerate@mintset.io>
2025-01-10 11:53:13 +0100

    to 3.15.0

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250110_103814 from './20250110_103814';
+import * as migration_20250110_105122 from './20250110_105122';
 
 export const migrations = [
   {
-    up: migration_20250110_103814.up,
-    down: migration_20250110_103814.down,
-    name: '20250110_103814'
+    up: migration_20250110_105122.up,
+    down: migration_20250110_105122.down,
+    name: '20250110_105122'
   },
 ];
